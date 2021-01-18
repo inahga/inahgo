8d1fbdcc25efdc7a5febe3b20b0b48daad27a4f7
Ameer Ghani <inahga@gmail.com>
2021-01-18 16:24:14 -0600

    build: mage init

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/inahga/inahgo
 
 go 1.15
+
+require github.com/magefile/mage v1.11.0 // indirect
---
+++ magefile.go
@@ -0,0 +1,13 @@
+// +build mage
+
+package main
+
+import "fmt"
+
+func RunDep() {
+	fmt.Println("runDep")
+}
+
+func BuildDep() {
+	fmt.Println("buildDep")
+}
