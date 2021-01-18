ab6ba77cf2e3e67fea15d1548023f20945d45365
Ameer Ghani <inahga@gmail.com>
2021-01-18 16:52:17 -0600

    build: add runtime and compile time dependencies

--- magefile.go
+++ magefile.go
@@ -2,12 +2,60 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
 
-func RunDep() {
-	fmt.Println("runDep")
+	"github.com/inahga/inahgo/pkg/distro"
+)
+
+// RunDep provides a list of runtime dependencies, based on the running OS/distribution.
+func RunDep() error {
+	packman, err := distro.GetPackageManager()
+	if err != nil {
+		return err
+	}
+
+	var pkgs []string
+	switch packman {
+	case distro.Yum:
+		pkgs = []string{
+			"libimobiledevice",
+		}
+	case distro.Apt:
+		pkgs = []string{
+			"libimobiledevice6",
+		}
+	default:
+		return fmt.Errorf("unknown or unsupported platform for runDep: %s/%s",
+			distro.Detect(), packman.Command())
+	}
+	fmt.Println(strings.Join(pkgs, " "))
+	return nil
 }
 
-func BuildDep() {
-	fmt.Println("buildDep")
+// BuildDep provides a list of compile-time dependencies, based on the running
+// OS/distribution.
+func BuildDep() error {
+	packman, err := distro.GetPackageManager()
+	if err != nil {
+		return err
+	}
+
+	var pkgs []string
+	switch packman {
+	case distro.Yum:
+		pkgs = []string{
+			"libimobiledevice-devel",
+		}
+	case distro.Apt:
+		pkgs = []string{
+			"libimobiledevice-dev",
+		}
+	default:
+		return fmt.Errorf("unknown or unsupported platform for runDep: %s/%s",
+			distro.Detect(), packman.Command())
+	}
+	fmt.Println(strings.Join(pkgs, " "))
+	return nil
 }
